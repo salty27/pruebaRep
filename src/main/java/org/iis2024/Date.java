6ef16c4fd2a4becfb090953115b04a18d69ebe48
mariamelll <mariamellahiani@uma.es>
2024-03-12 09:30:40 +0100

    added variable

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,7 +19,7 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
-
+  private int owo;
   /**
    * Retrieves the day component of this date.
    *
